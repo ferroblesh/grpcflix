38bcbe03f3d43558db0cd968920be9c5b695412d
Fernando Robles <fernando.robles@omnitracs.com>
2021-08-13 17:41:34 -0500

    Add User movie suggestion grpc client

--- aggregator-service/src/main/java/com/grpcflix/agreggator/controller/AggregatorController.java
+++ aggregator-service/src/main/java/com/grpcflix/agreggator/controller/AggregatorController.java
@@ -2,6 +2,8 @@ package com.grpcflix.agreggator.controller;
 
 import com.grpcflix.agreggator.dto.RecommendedMovie;
 import com.grpcflix.agreggator.dto.UserGenre;
+import com.grpcflix.agreggator.service.UserMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,13 +11,16 @@ import java.util.List;
 @RestController
 public class AggregatorController {
 
+    @Autowired
+    private UserMovieService userMovieService;
+
     @GetMapping("/user/{loginId}")
     public List<RecommendedMovie> getMovies(@PathVariable String loginId) {
-
+        return this.userMovieService.getUserMovieSuggestions(loginId);
     }
 
     @PutMapping("/user")
     public void setUserGenre(@RequestBody UserGenre userGenre) {
-
+        this.userMovieService.setUserGenre(userGenre);
     }
 }
--- aggregator-service/src/main/java/com/grpcflix/agreggator/service/UserMovieService.java
+++ aggregator-service/src/main/java/com/grpcflix/agreggator/service/UserMovieService.java
@@ -1,12 +1,15 @@
 package com.grpcflix.agreggator.service;
 
+import com.ferroblesh.grpcflix.common.Genre;
 import com.ferroblesh.grpcflix.movie.MovieSearchRequest;
 import com.ferroblesh.grpcflix.movie.MovieSearchResponse;
 import com.ferroblesh.grpcflix.movie.MovieServiceGrpc;
+import com.ferroblesh.grpcflix.user.UserGenreUpdateRequest;
 import com.ferroblesh.grpcflix.user.UserResponse;
 import com.ferroblesh.grpcflix.user.UserSearchRequest;
 import com.ferroblesh.grpcflix.user.UserServiceGrpc;
 import com.grpcflix.agreggator.dto.RecommendedMovie;
+import com.grpcflix.agreggator.dto.UserGenre;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +32,10 @@ public class UserMovieService {
         return movieSearchResponse.getMoviesList().stream()
                 .map(movie -> new RecommendedMovie(movie.getTitle(),movie.getYear(),movie.getYear()))
                 .collect(Collectors.toList());
+    }
+
+    public void setUserGenre(UserGenre userGenre) {
+        UserResponse userResponse = this.userStub.updateUserGenre(UserGenreUpdateRequest.newBuilder().setGenre(Genre.valueOf(userGenre.getGenre().toUpperCase()))
+                .setLoginId(userGenre.getLoginId()).build());
     }
 }
